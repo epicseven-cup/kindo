5fab74078c5d49ced29a9817ecb9042f4f7dfb08
jacky <jackylin616@gmail.com>
2026-04-19 00:16:43 -0500

    update"

--- src/build.zig
+++ src/build.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.s
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const kindo = b.addModule("kindo", .{ .root_source_file = b.path("src/allocator.zig"), .target = target, .optimize = optimize });
+
+    const kindo_daemon = b.addExecutable(.{
+        .name = "kindo-daemon",
+        .root_module = "src/main.zig",
+    });
 }
