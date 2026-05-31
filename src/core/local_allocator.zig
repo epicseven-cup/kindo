cac6ee8d1f4989016d483bc5c1d9fb6300d897ca
jacky <jackylin616@gmail.com>
2026-05-31 02:17:28 -0500

    update local allcoator

--- src/core/local_allocator.zig
+++ src/core/local_allocator.zig
@@ -1,11 +1,58 @@
 const std = @import("std");
 // zig fmt: off
+
+const AllocatorError = error {
+    InvalidFree,
+};
+
 pub const LocalAllocator = struct {
     limit: usize,
     allocator: std.mem.Allocator,
+    mutex: std.Thread.Mutex,
     memory_used: usize,
 
+    pub fn init(limit: usize, allocator_interface: std.mem.Allocator) LocalAllocator {
+        return LocalAllocator {
+            .limit  = limit,
+            .allocator = allocator_interface,
+            .mutex = std.Thread.Mutex{},
+            .memory_used =  0,
+        };
+    }
+
+    fn vtableAlloc(ctx: *anyopaque, len:usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
+        const self: *LocalAllocator = @ptrCast(@alignCast(ctx));
+        _ = ptr_align; _ = ret_addr;
+        const block = self.alloc(len) catch return null;
+        return @ptrCast(block);
+    }
+
+    fn vtableFree(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
+        const self: *LocalAllocator = @ptrCast(@alignCast(ctx));
+        _ = buf_align; _ = ret_addr;
+        self.free(buf) catch {};
+    }
+
+    fn vtableResize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+        _ = ctx; _ = buf; _ =  buf_align; _ = new_len; _ = ret_addr;
+        return false;
+    }
+
+    pub fn allocator(self: *LocalAllocator) std.mem.Allocator {
+        return std.mem.Allocator {
+            .ptr = self,
+            .vtable = &.{
+                .alloc = vtableAlloc,
+                .free = vtableFree,
+                .resize = vtableResize,
+            },
+        };
+    }
+
+
     pub fn alloc(self: *LocalAllocator, size: usize) ![]u8 {
+        // Locking the allocator so we dont get race conditions when allocating/deallocating memory checking limits
+        self.mutex.lock(); defer self.mutex.unlock();
         if (self.memory_used + size > self.limit) {
             return error.OutOfMemory;
         }
@@ -14,7 +61,12 @@ pub const LocalAllocator = struct {
         return block;
     }
 
-    pub fn free(self: *LocalAllocator, ptr: []u8) void {
+    pub fn free(self: *LocalAllocator, ptr: []u8) !void {
+        self.mutex.lock(); defer self.mutex.unlock();
+        if (self.memory_used < ptr.len) {
+            std.debug.print("Memory used is less than the size of the block freed {} < {} \n", .{ self.memory_used, ptr.len });
+            return AllocatorError.InvalidFree;
+        }
         self.allocator.free(ptr);
         self.memory_used -= ptr.len;
     }
